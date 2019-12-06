63dc21cfcb2005fc0f6f15dade10d7efa9a8e935
Ghiles FEGHOUL <ghiles.feghoul@gmail.com>
2019-12-06 14:30:32 +0100

    Partie 2 du blog openclassrooms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { AppComponent } from './app.component'
 import { PostListComponent } from './post-list/post-list.component'
 import { PostListItemComponent } from './post-list-item/post-list-item.component'
+import { NewPostComponent } from './new-post/new-post.component';
 import { PostService } from './services/post.service'
 
 
 const appRoutes: Routes = [
   {path: 'posts', component: PostListComponent},
+  {path: 'new', component: NewPostComponent},
   {path: '', component: PostListComponent},
   {path: '**', redirectTo: 'posts'}
 ]
@@ -21,6 +23,7 @@ const appRoutes: Routes = [
     AppComponent,
     PostListComponent,
     PostListItemComponent,
+    NewPostComponent
   ],
   imports: [
     FormsModule,
